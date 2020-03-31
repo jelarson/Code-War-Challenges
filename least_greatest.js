67738b319310610824a5a1883df12362b7bfa4ec
jelarson <jesslarson93@gmail.com>
2020-03-31 15:29:50 -0600

    returns highest and lowest in string

---
+++ least_greatest.js
@@ -0,0 +1,28 @@
+// In this little assignment you are given a string of
+// space separated numbers, and have to return the 
+// highest and lowest number.
+
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+
+function highAndLow(numbers) {
+  arr = []
+  numbers.split(' ').forEach(num => {
+    arr.push(Number(num))
+  })
+  return `${Math.max(...arr)} ${Math.min(...arr)}`
+}
+
+console.log(highAndLow("1 2 -3 4 5")); // return "5 -3"
+
+function highAndLow1(numbers) {
+  // arr = []
+  // numbers.split(' ').forEach(num => {
+  //   arr.push(Number(num))
+  // })
+  nums = numbers.split(' ')
+  return `${Math.max(...nums)} ${Math.min(...nums)}`
+}
+
+console.log(highAndLow1('1 2 -3 4 5'));
+
+// .sort((a, b) => a - b)
