5eb77e64dc9ea608127512cc4983585abdd5569e
jelarson <jesslarson93@gmail.com>
2020-05-21 14:25:22 -0600

    function checks if values can make a triangle

---
+++ is_triangle.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/56606694ec01347ce800001b/train/javascript
+
+function isTriangle(a, b, c) {
+  let valArr = [a, b, c];
+  let maxNum = Math.max(...valArr);
+  valArr.splice(valArr.indexOf(maxNum), 1);
+  return valArr.reduce((a, b) => a + b, 0) > maxNum ? true : false;
+}
+
+console.log(isTriangle(7, 2, 2));
+console.log(isTriangle(1, 2, 2));
