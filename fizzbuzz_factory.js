5dab4e75ef80c1ba266768263148711f4155bc88
jelarson <jesslarson93@gmail.com>
2020-05-28 10:55:59 -0600

    fizzbuzz factory

---
+++ fizzbuzz_factory.js
@@ -0,0 +1,60 @@
+// https://www.codewars.com/kata/5645d1a4d907bd6009000052/train/javascript
+
+// var myFizz = fizzBuzzFactory([[3, "Fizz"], [5, "Buzz"], [15, "FizzBuzz"]])
+// myFizz(3)  === "Fizz"
+// myFizz(4)  === "4"
+// myFizz(5)  === "Buzz"
+// myFizz(15) === "FizzBuzz"
+
+function fizzBuzzFactory(arr, arg) {
+  let valueArr = [];
+  let wordArr = [];
+
+  arr.forEach((item) => {
+    valueArr.push(item[0]);
+    wordArr.push(item[1]);
+  });
+
+  let maxInd = valueArr.indexOf(Math.max(...valueArr));
+  let minInd = valueArr.indexOf(Math.min(...valueArr));
+  let midInd = 0;
+  if (maxInd + minInd === 1) {
+    midInd += 2;
+  }
+  if (maxInd + minInd === 2) {
+    midInd += 1;
+  }
+
+  if (arg % Number(valueArr[maxInd]) === 0) {
+    return wordArr[maxInd];
+  }
+  if (
+    arg % Number(valueArr[midInd]) === 0 &&
+    arg % Number(valueArr[maxInd]) !== 0
+  ) {
+    return wordArr[midInd];
+  }
+  if (
+    arg % Number(valueArr[minInd]) === 0 &&
+    arg % Number(valueArr[maxInd]) !== 0 &&
+    arg % Number(valueArr[midInd]) !== 0
+  ) {
+    return wordArr[minInd];
+  } else {
+    return String(arg);
+  }
+}
+
+console.log(
+  fizzBuzzFactory(
+    [
+      [3, "Fizz"],
+      [5, "Buzz"],
+      [15, "FizzBuzz"],
+    ],
+    16
+  )
+);
+
+console.log(Math.max(3, 5, 15, 21, 99, 100));
+console.log(Math.min(3, 5, 15, 21, 99, 100));
---
+++ ryan_challenge.js
@@ -0,0 +1,29 @@
+// argument number
+// levels of pyramid
+// print astericks
+//   *
+//  ***
+// *****
+
+function pyramid(num) {
+  for (let i = 1; i <= num; i++) {
+    let ast = -1 + i * 2;
+    let dif = num - i;
+    const level = [];
+    level.push(" ".repeat(dif));
+    level.push("*".repeat(ast));
+    console.log(level.join(""));
+  }
+}
+
+// pyramid(3);
+pyramid(27);
+
+let str = "string";
+console.log(str);
+str = str.slice(1, 2);
+console.log(str);
+
+let setty = new Set([2, 3, 4, 3]);
+console.log(Array.from("hello", (letter) => letter.toUpperCase()));
+console.log(setty);
