ac2d378fd2d15cda8e0b3b3dcfa0b1cbdf5cae1e
jelarson <jesslarson93@gmail.com>
2020-06-01 13:48:29 -0600

    fixed bracket

--- phone_num.js
+++ phone_num.js
@@ -5,6 +5,6 @@ function createPhoneNumber(numbers) {
   return (
     "(" + nums.slice(0, 3) + ") " + nums.slice(3, 6) + "-" + nums.slice(6, 10)
   );
-
+}
 
 console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]));
