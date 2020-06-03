7779bdc9604bef206b819cc4d9620b209c52b24b
jelarson <jesslarson93@gmail.com>
2020-06-03 11:24:31 -0600

    additional solutions

--- iq_test.js
+++ iq_test.js
@@ -19,6 +19,31 @@ function iqTest(numbers) {
 
 console.log(iqTest("2 4 7 8 10"));
 
+function iqTest1(numbers) {
+  let oddArr = numbers.split(" ").filter((num) => {
+    return num % 2 === 1 ? true : false;
+  });
+  let evenArr = numbers.split(" ").filter((num) => {
+    return num % 2 === 1 ? false : true;
+  });
+  let minArr = oddArr.length < evenArr.length ? oddArr : evenArr;
+  return numbers.split(" ").indexOf(minArr[0]) + 1;
+}
+
+console.log(iqTest1("2 4 7 8 10"));
+
+function iqTest2(numbers) {
+  let oddArr = [];
+  let evenArr = [];
+  numbers.split(" ").forEach((num) => {
+    num % 2 === 0 ? evenArr.push(num) : oddArr.push(num);
+  });
+  let minArr = oddArr.length < evenArr.length ? oddArr : evenArr;
+  return numbers.split(" ").indexOf(minArr[0]) + 1;
+}
+
+console.log(iqTest2("2 4 7 8 10"));
+
 // Top Answer
 
 function iqTest(numbers) {
