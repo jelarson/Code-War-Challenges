1516a8feebbf0aaf7a7c43d6035858dd38e8e5c3
jelarson <jesslarson93@gmail.com>
2020-06-08 12:44:31 -0600

    including top answer

--- order_names.js
+++ order_names.js
@@ -20,3 +20,16 @@ function list(names) {
     }`;
   }
 }
+
+// Top answer
+function list(names) {
+  return names.reduce(function (prev, current, index, array) {
+    if (index === 0) {
+      return current.name;
+    } else if (index === array.length - 1) {
+      return prev + " & " + current.name;
+    } else {
+      return prev + ", " + current.name;
+    }
+  }, "");
+}
