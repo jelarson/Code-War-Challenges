0bde8e9bcbf21148455df488ea31579ac6e7ca36
jelarson <jesslarson93@gmail.com>
2020-09-14 10:41:28 -0600

    top answers added

--- longest_repetition.js
+++ longest_repetition.js
@@ -25,3 +25,33 @@ console.log(longestRepetition("bbbaaabaaaa"))
 // console.log(longestRepetition("cbdeuuu900"))
 // console.log(longestRepetition("abbbbb"))
 // console.log(longestRepetition("aabb"))
+
+// top answer
+
+const longestRepetition = s => {
+  let max = 0;
+  let char = ''; 
+  for (let i = 1, l = s.length, c = 1; i <= l; i++) {
+    if (s[i] === s[i - 1]) c++;
+    else {
+      if (c > max) {
+        max = c;
+        char = s[i - 1];   
+      }
+      c = 1;
+    }
+  }
+  return [char, max];
+}
+
+// top answer - sort and splice
+const longestRepetition = s => s ?
+  s.match(/(.)\1*/g)
+    .map((s, i) => [s[0], s.length, i])
+    .sort((a, b) => (b[1] - a[1]) || (a[2] - b[2]))[0]
+    .splice(0, 2) :
+  ['', 0];
+
+// regex top answer
+
+const longestRepetition = s => (s.match(/(.)(\1*)/g) || []).reduce((a,e) => e.length > a[1] ? [e[0],e.length] : a,['',0]);
