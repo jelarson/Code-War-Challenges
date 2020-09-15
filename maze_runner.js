3e12e826073318b381685646ebfbaf2ba0157797
jelarson <jesslarson93@gmail.com>
2020-09-15 11:24:07 -0600

    maze runner solution - did the directions solve the maze?

--- maze_runner.js
+++ maze_runner.js
@@ -9,7 +9,51 @@ var maze = [[1,1,1,1,1,1,1],
             [1,2,1,0,1,0,1]];
 
 function mazeRunner(maze, directions) {
-  // Code here
+  let vert = 0
+  let horiz = 0
+  maze.forEach((row, vertIdx) => {
+    row.forEach((num, horizIdx) => {
+      if (num === 2){
+        vert = vertIdx
+        horiz = horizIdx
+      }
+    })
+  })
+  directions.forEach((direction) => {
+    if (vert === maze.length || horiz === maze.length || horiz < 0 || vert < 0){
+      return 'Dead'
+    }
+    if (maze[vert][horiz] === 1){
+      return 'Dead'
+    }
+    if (maze[vert][horiz] === 3){
+      return 'Finish'
+     }
+    if (direction === 'N'){
+      vert--
+    }
+    if (direction === 'E'){
+      horiz++
+    }
+    if (direction === 'S'){
+      vert++
+    }
+    if (direction === 'W'){
+      horiz--
+    }
+  })
+  if (vert === maze.length || horiz === maze.length || horiz < 0 || vert < 0){
+    return 'Dead'
+  }
+  if (maze[vert][horiz] === 1){
+    return 'Dead'
+  }
+  if (maze[vert][horiz] === 3){
+    return 'Finish'
+   }
+  return 'Lost'
 }
 
 console.log(mazeRunner(maze, ["N","N","N","N","N","E","E","S","S","E","E","N","N","E"]))
+console.log(mazeRunner(maze, ["N","N","N","N","N","E","E","E","E","E","W","W"]))
+console.log(mazeRunner(maze, ["N","N","N","N","N","E","E","S","S","S","S","S","S"]))
