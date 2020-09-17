72d3f08d8c9799f2f1a88ab3154267ee18bb7fc1
jelarson <jesslarson93@gmail.com>
2020-09-17 12:12:10 -0600

    multiplication tables with top answer

--- mult_table.js
+++ mult_table.js
@@ -21,3 +21,18 @@ multiplicationTable = function(size) {
 }
 
 console.log(multiplicationTable(3))
+
+// top answer
+
+multiplicationTable = function(size) {
+  var result = [];
+
+  for (var i = 0; i < size; i++) {
+    result[i] = [];
+    for(var j = 0; j < size; j++) {
+      result[i][j] = (i + 1) * (j + 1);
+    }
+  }
+  
+  return result
+}
