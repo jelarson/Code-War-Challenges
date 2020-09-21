a3b0257441b561651f9636d428e5f0699d3bece4
jelarson <jesslarson93@gmail.com>
2020-09-21 13:01:51 -0600

    other top answers added

--- backspace.js
+++ backspace.js
@@ -16,3 +16,28 @@ function cleanString(s) {
 console.log(cleanString("abc#d##c"))
 console.log(cleanString('abc####d##c#'))
 
+// simple for loop solution
+
+function clean_string(s) {
+  const result = []
+  for (const c of s) {
+    if (c === "#") {
+      result.pop()
+    } else {
+      result.push(c)
+    }
+  }
+  return result.join("")
+}
+
+// one liner with reducer
+
+clean_string = s => s.split('').reduce((r, e) => e == '#' ? r.slice(0, -1) : r + e, '');  
+
+// map solution
+
+function cleanString(s) {
+  let result = [];
+  [...s].map((char) => (char === "#" ? result.pop() : result.push(char)));
+  return result.join("");
+}
