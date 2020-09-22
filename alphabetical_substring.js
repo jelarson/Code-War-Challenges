c05916f56997da27e49ba5100b6e67fd7b73f2d6
jelarson <jesslarson93@gmail.com>
2020-09-22 16:55:17 -0600

    top answers added

--- alphabetical_substring.js
+++ alphabetical_substring.js
@@ -25,3 +25,27 @@ console.log(longest('asd'))
 console.log(longest('nab'))
 console.log(longest('asdfaaaabbbbcttavvfffffdf'))
 
+// top answer - one liner with regex
+
+longest = s => s.match(/a*b*c*d*e*f*g*h*i*j*k*l*m*n*o*p*q*r*s*t*u*v*w*x*y*z*/g).reduce((a, b) => a.length >= b.length ? a : b);
+
+
+// for loop top answer
+
+function longest(str) {
+  let max = 0;
+  let cur = 0;
+  let pos = 0;
+  for (let i = 1; i < str.length; ++i) {
+    if (str[i-1] <= str[i]) {
+      ++cur;
+      if (cur > max) {
+        max = cur;
+        pos = i - max;
+      }
+    } else {
+      cur = 0;
+    }
+  }
+  return str.slice(pos, max+1+pos);
+}
