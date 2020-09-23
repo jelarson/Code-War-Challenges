9ae4b77764f5ad956e8086f13a6bfd7b965fb330
jelarson <jesslarson93@gmail.com>
2020-09-23 13:41:51 -0600

    top answers added

--- sum_consec.js
+++ sum_consec.js
@@ -29,3 +29,28 @@ function sumConsecutives(s) {
 
 console.log(sumConsecutives([1,4,4,4,0,4,3,3,1]))
 console.log(sumConsecutives([1,1,7,7,3]))
+
+// top answer with reducer
+
+function sumConsecutives(s) {
+  return s.reduce(function(prev, curr, i, arr) {
+    if(curr != arr[i - 1]) prev.push(curr);
+    else prev[prev.length - 1] += curr;
+    return prev;
+  }, []);
+}
+
+// top answer - for loop
+
+function sumConsecutives(s) {
+  var sum = 0,
+  sums = [];
+  for( var i = 0; i < s.length; i++){
+    sum += s[i];
+    if(s[i] != s[i+1]){
+      sums.push(sum);
+      sum = 0;
+    }
+  }
+  return sums;
+}
