23e85b7d774efb84bd52544fa48fae30735b960b
jelarson <jesslarson93@gmail.com>
2020-09-23 12:57:23 -0600

    My working solution

--- frequency_sort.js
+++ frequency_sort.js
@@ -18,21 +18,23 @@ function solve(arr){
   })
   const keys = Object.keys(obj)
   const vals = Object.values(obj)
-  let currentHigh = Math.max(...vals)
-  const currentEval = []
-
-  keys.forEach(key => {
-    if (obj[key] === currentHigh){
-      currentEval.push(key)
-    }
-  })
-  currentEval.sort().forEach(value => {
-    for (let i = 0; i < currentHigh; i++){
-      ans.push(Number(value))
-      currentHigh = Math.max(...vals.slice(0, vals.indexOf(currentHigh)), ...vals.slice(vals.indexOf(currentHigh)+1))
-    }
+  const hello = [...new Set(vals.sort(function(a,b){return a-b}).reverse())];
+  
+  hello.forEach(value => {
+    const currentEval = []
+    keys.forEach(key => {
+      if (obj[key] === value){
+        currentEval.push(Number(key))
+      }
+    })
+    currentEval.sort(function(a,b){return a-b}).forEach(item => {
+      for (let i = 0; i < value; i++){
+        ans.push(item)
+      }
+    })
   })
   return ans
 }
+
 
 console.log(solve([2,3,5,3,7,9,5,3,7]))
