c50661bc61256cdf6a15105f67e8808081bf2dd8
jelarson <jesslarson93@gmail.com>
2020-09-23 12:58:54 -0600

    top answer added

--- frequency_sort.js
+++ frequency_sort.js
@@ -36,5 +36,12 @@ function solve(arr){
   return ans
 }
 
-
 console.log(solve([2,3,5,3,7,9,5,3,7]))
+
+// top answer
+
+function solve(arr){
+  var r={}
+  for(var n of arr) r[n]=r[n]+1||1
+  return arr.slice().sort((a,b)=>r[b]-r[a]||a-b)
+}
