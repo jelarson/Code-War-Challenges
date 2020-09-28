82eab53e1e6b9365b744512207489ce5a9a8a057
jelarson <jesslarson93@gmail.com>
2020-09-28 12:41:44 -0600

    morse code translator

---
+++ decode_morse.js
@@ -0,0 +1,48 @@
+// https://www.codewars.com/kata/54b724efac3d5402db00065e/train/javascript
+
+decodeMorse = function(morseCode){
+  const obj = {
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '...---...': 'sos',
+    '-.-.--': '!',
+    '.-.-.-': '.'
+  }
+  const ans = []
+  morseCode.split('  ').forEach(word => {
+    const morseWord = []
+    word.split(' ').forEach(letter => {
+      morseWord.push(obj[letter])
+    })
+    ans.push(morseWord.join(''))
+  })
+  return ans.join(' ').toUpperCase().trim()
+}
+
+console.log(decodeMorse('.... . -.--   .--- ..- -.. .'))
+console.log(decodeMorse('... --- ...'))
+console.log(decodeMorse('      ...---... -.-.--   - .... .   --.- ..- .. -.-. -.-   -... .-. --- .-- -.   ..-. --- -..-   .--- ..- -- .--. ...   --- ...- . .-.   - .... .   .-.. .- --.. -.--   -.. --- --. .-.-.-  '))
