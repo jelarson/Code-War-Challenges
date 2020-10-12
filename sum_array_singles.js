5be84c5bdd8eb8bc1067a8ada6b736c629703067
jelarson <jesslarson93@gmail.com>
2020-10-12 15:04:43 -0600

    top answers added

--- sum_array_singles.js
+++ sum_array_singles.js
@@ -2,12 +2,32 @@
 
 function repeats(arr){
   const ans = []
-  arr.forEach((letter)=> {
-    if (arr.indexOf(letter) === arr.lastIndexOf(letter)){
-      ans.push(letter)
+  arr.forEach((num)=> {
+    if (arr.indexOf(num) === arr.lastIndexOf(num)){
+      ans.push(num)
     }
   })
   return ans[0] + ans[1]
   };
 
 console.log(repeats([4,5,7,5,4,8]))
+
+// top answer
+
+// one liner
+
+function repeats(arr){
+  return arr.filter(v => arr.indexOf(v) === arr.lastIndexOf(v)).reduce((a,b) => a + b, 0);
+};
+
+// my answer with summing during loop
+
+function repeats(arr){
+  let ans = 0
+  arr.forEach((num)=> {
+    if (arr.indexOf(num) === arr.lastIndexOf(num)){
+      ans+=num
+    }
+  })
+  return ans
+  };
