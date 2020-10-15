cb92a73a4a602abb6f344d490b48102b73389166
jelarson <jesslarson93@gmail.com>
2020-10-15 12:49:16 -0600

    top answers added

--- range_extraction.js
+++ range_extraction.js
@@ -46,3 +46,37 @@ function solution(list){
  "-6,-3-1,3-5,7-11,14,15,17-20"
 
  console.log(solution([ -91, -88, -87, -86, -83, -82, -81, -80, -79, -76, -73, -72, -71, -68 ]))
+
+ // top answer - while loop
+
+ function solution(list){
+  for(var i = 0; i < list.length; i++){
+     var j = i;
+     while(list[j] - list[j+1] == -1) j++;
+     if(j != i && j-i>1) list.splice(i, j-i+1, list[i] +'-'+list[j]);
+ }
+ return list.join();
+}
+
+// top answer map and filter
+
+function solution(nums){
+  nums = nums.map((v, i) => nums[i - 1] == v - 1 && nums[i + 1] == v + 1 ? '-' : v);
+     return nums.filter((v, i) => v != '-' || nums[i - 1] != '-').join(',').replace(/,-,/g, '-');
+ }
+
+ // top answer reducer
+
+ function solution(list){
+  return list
+    .reduce((acc, n, i, arr) => {
+      if(n !== arr[i - 1] + 1) {
+        acc.push([n]);
+      } else {
+        acc[acc.length - 1].push(n);
+      }
+      return acc;
+    }, [])
+    .map(arr => arr.length > 2 ? `${arr[0]}-${arr[arr.length-1]}` : arr)
+    .join(',');
+}
