937df188aaa4a74d4a4df4013c2b185b9632425a
jelarson <jesslarson93@gmail.com>
2020-10-19 15:44:24 -0600

    top answers added

--- life_path.js
+++ life_path.js
@@ -19,3 +19,22 @@ function lifePathNumber(dateOfBirth) {
 }
 
 console.log(lifePathNumber('1879-03-14'))
+
+// top answer
+
+function lifePathNumber(dateOfBirth) {
+  return dateOfBirth.replace(/\D/g,'') % 9 || 9;
+}
+
+// top answer
+
+function lifePathNumber(dateOfBirth) {
+  var num = dateOfBirth.split("-").join("");
+  
+  do{
+    num = (""+num).split("").reduce((p,c) => +p+ +c);
+  }while(num > 9);
+  
+  return num;
+}
+
