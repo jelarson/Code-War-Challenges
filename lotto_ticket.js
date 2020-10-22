51a3524d984c65ba766b3cdf399f3989b158887d
jelarson <jesslarson93@gmail.com>
2020-10-22 11:23:59 -0600

    top answers added

--- lotto_ticket.js
+++ lotto_ticket.js
@@ -16,3 +16,24 @@ function bingo(ticket, win){
 // console.log(bingo([['ABC', 65], ['HGR', 74], ['BYHT', 74]], 1))
 // console.log(bingo([['HGTYRE', 74], ['BE', 66], ['JKTY', 74]], 3))
 console.log(bingo([ [ 'ZXTNVSTT', 73 ], [ 'YOYFFA', 89 ] ], 2))
+
+// ryan's answer
+
+function bingo(ticket, win){
+  const wins = ticket.reduce((total, next) => total += +[...next[0]].some(char => char.charCodeAt() === next[1]),0)
+  return wins >= win ? 'Winner!' : 'Loser!'
+}
+
+// top answer
+
+function bingo(ticket, win){
+  if(ticket.filter(a => a[0].split('').some(b => b.charCodeAt(0) == a[1])).length >= win)
+  {
+    return "Winner!";
+  }
+  return "Loser!";
+}
+
+// one liner
+
+const bingo = (ticket, win) =>  ticket.reduce((s,[e,c])=> [...e].some(l=> l.charCodeAt(0) == c) ? s+1: s  ,0) >= win ? "Winner!" : "Loser!";
