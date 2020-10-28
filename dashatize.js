e87047292699758f9c21e6cd65d5cfa74c022b29
jelarson <jesslarson93@gmail.com>
2020-10-28 12:55:20 -0600

    top answers added

--- dashatize.js
+++ dashatize.js
@@ -29,3 +29,37 @@ function dashatize(num) {
 console.log(dashatize(274))
 console.log(dashatize(5311))
 console.log(dashatize(86320))
+
+// top answer - regex
+function dashatize(num) {
+  return String(num)
+    .replace(/([13579])/g, "-$1-")
+    .replace(/--+/g, "-")
+    .replace(/(^-|-$)/g, "")
+}
+
+// no regex
+function dashatize(num) {
+  return num = num.toString().split("").map(function(c){
+    return c % 2 ? "-" + c + "-" :  c ;
+  }).join("").split("-").filter(a => a != "").join("-");
+  
+};
+
+function dashatize(num) {
+  var dashed=Math.abs(num).toString().split('').map(a=> +a%2==1? '-'+a+'-':a).join('');
+  if (dashed[0]=='-') {
+    dashed=dashed.slice(1);
+  } 
+  if (dashed[dashed.length-1]=='-') {
+    dashed=dashed.slice(0,dashed.length-1);
+  }
+  for(var i=0;i<dashed.length;i++) {
+    if (dashed[i]=='-') {
+      if (dashed[i+1]=='-') {
+        dashed=dashed.substring(0,i+1)+dashed.substring(i+2);
+      }
+    }
+  }
+  return dashed;
+};
