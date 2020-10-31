b50b44220ea6d18a19bb379cb3a5154a84cf3dbf
jelarson <jesslarson93@gmail.com>
2020-10-30 17:12:00 -0700

    top answers added

--- midtown_nav.js
+++ midtown_nav.js
@@ -1,7 +1,6 @@
 // https://www.codewars.com/kata/59665001dc23af735700092b/train/javascript
 
 function midtownNav(start,end) {
-  // const startNums = start.replace(/([a-z])/gi, '')
   const startNums = start.match(/([0-9])\w+/gi,).map(el => el.replace(/([a-z])/gi, ''))
   const endNums = end.match(/([0-9])\w+/gi,).map(el => el.replace(/([a-z])/gi, ''))
   const ans = [startNums[0] - endNums[0], startNums[1] - endNums[1]]
@@ -11,3 +10,24 @@ function midtownNav(start,end) {
 }
 
 console.log(midtownNav("8th Ave and W 38th St", "7th Ave and W 36th St"))
+
+// top answer
+
+function midtownNav(...addrs) {
+  
+  const [ start, end ] = addrs.map(addr => addr.match(/(\d)\D*(\d+)/));
+
+  const dir = (dist, ...names) => Math.abs(dist) + ' blocks ' + names[+(dist < 0)];
+  
+  return 'Walk ' + dir(end[2] - start[2], 'north', 'south') 
+      + ', and ' + dir(end[1] - start[1], 'west', 'east');
+  
+}
+
+// top short answer
+
+function midtownNav(start, end) {
+  const [ax, ay, bx, by] = (start + end).match(/\d+/g)
+  const dx = ax - bx, dy = ay - by
+  return `Walk ${Math.abs(dy)} blocks ${dy > 0 ? "south" : "north"}, and ${Math.abs(dx)} blocks ${dx > 0 ? "east" : "west"}`
+}
