ec4734bdacc09a466d5dfde897693820edc6cdce
jelarson <jesslarson93@gmail.com>
2020-11-02 15:46:44 -0700

    reduce contestants array

---
+++ elimination_tourny.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/5f631ed489e0e101a70c70a0/train/javascript
+
+const tourney = array => {
+  if (array.length === 1){
+    return array
+  }
+  const newArr = []
+  for (let i = 1; i < array.length; i+=2){
+    array[i] >= array[i - 1] ? newArr.push(array[i]) : newArr.push(array[i-1])
+  }
+  return tourney(newArr)
+}
+
+console.log(tourney([9, 5, 4, 7, 6, 3, 8, 2]))
