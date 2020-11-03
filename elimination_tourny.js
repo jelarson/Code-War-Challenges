fa4011cbac8b152eaf153a4c54496b7c4887b713
jelarson <jesslarson93@gmail.com>
2020-11-03 09:06:19 -0700

    passes test cases - edge cases with odd nums

--- elimination_tourny.js
+++ elimination_tourny.js
@@ -19,11 +19,12 @@ function tourney(array, array2 = []) {
   for (let i = 1; i < array.length; i+=2){
     array[i] >= array[i - 1] ? newArr.push(array[i]) : newArr.push(array[i-1])
   }
-  if (array.length > 1 && array.length %2 !== 0){
-    array.unshift(array[array.length-1])
+  if (newArr.length > 1 && newArr.length %2 !== 0){
+    newArr.unshift(array[array.length-1])
   }
   return tourney(newArr, array2)
 }
 
 // console.log(tourney([9, 5, 4, 7, 6, 3, 8, 2]))
-console.log(tourney([9, 5, 4, 7, 6, 3, 8]))
+// console.log(tourney([9, 5, 4, 7, 6, 3, 8]))
+console.log(tourney([ 59, 62, 51, 56, 29, 70, 92, 49, 58, 39, 1, 3, 67, 82 ]))
