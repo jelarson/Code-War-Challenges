a38066217d3f23a05c61fd8f23b0250bb19c6011
jelarson <jesslarson93@gmail.com>
2020-11-03 08:32:52 -0700

    restructured function

--- elimination_tourny.js
+++ elimination_tourny.js
@@ -1,6 +1,16 @@
 // https://www.codewars.com/kata/5f631ed489e0e101a70c70a0/train/javascript
 
-const tourney = array => {
+// const tourney = array => {
+//   if (array.length === 1){
+//     return array
+//   }
+//   const newArr = []
+//   for (let i = 1; i < array.length; i+=2){
+//     array[i] >= array[i - 1] ? newArr.push(array[i]) : newArr.push(array[i-1])
+//   }
+//   return tourney(newArr)
+// }
+function tourney(array, array2 = []) {
   if (array.length === 1){
     return array
   }
