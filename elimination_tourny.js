c4b7e9553267992a0e5bf37f0a76d19988e5aa65
jelarson <jesslarson93@gmail.com>
2020-11-03 08:57:19 -0700

    dealing with odd array lengths

--- elimination_tourny.js
+++ elimination_tourny.js
@@ -11,14 +11,19 @@
 //   return tourney(newArr)
 // }
 function tourney(array, array2 = []) {
+  array2.push(array)
   if (array.length === 1){
-    return array
+    return array2
   }
   const newArr = []
   for (let i = 1; i < array.length; i+=2){
     array[i] >= array[i - 1] ? newArr.push(array[i]) : newArr.push(array[i-1])
   }
-  return tourney(newArr)
+  if (array.length > 1 && array.length %2 !== 0){
+    array.unshift(array[array.length-1])
+  }
+  return tourney(newArr, array2)
 }
 
-console.log(tourney([9, 5, 4, 7, 6, 3, 8, 2]))
+// console.log(tourney([9, 5, 4, 7, 6, 3, 8, 2]))
+console.log(tourney([9, 5, 4, 7, 6, 3, 8]))
