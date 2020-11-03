6e9c1e257ac0c19a02a5c71744ab7bff9657be77
jelarson <jesslarson93@gmail.com>
2020-11-03 16:15:43 -0700

    working solution

--- elimination_tourny.js
+++ elimination_tourny.js
@@ -15,16 +15,27 @@ function tourney(array, array2 = []) {
   if (array.length === 1){
     return array2
   }
+  // if (array.length > 1 && array.length % 2 !== 0){
+  //   array.unshift(array[array.length -1])
+  // }
   const newArr = []
+  // let odd = false
   for (let i = 1; i < array.length; i+=2){
     array[i] >= array[i - 1] ? newArr.push(array[i]) : newArr.push(array[i-1])
+    if (i === array.length -2){
+      // odd = true
+      // newArr.pop()
+      newArr.unshift(array[array.length -1])
+    }
   }
-  if (newArr.length > 1 && newArr.length %2 !== 0){
-    newArr.unshift(array[array.length-1])
-  }
+  // if (newArr.length > 1 && odd){
+  //   newArr.unshift(array[array.length-1])
+  // }
+  
   return tourney(newArr, array2)
 }
 
 // console.log(tourney([9, 5, 4, 7, 6, 3, 8, 2]))
 // console.log(tourney([9, 5, 4, 7, 6, 3, 8]))
-console.log(tourney([ 59, 62, 51, 56, 29, 70, 92, 49, 58, 39, 1, 3, 67, 82 ]))
+// console.log(tourney([ 59, 62, 51, 56, 29, 70, 92, 49, 58, 39, 1, 3, 67, 82 ]))
+console.log(tourney([50,79,37,43,75,65,95,39,39,34,85,68,47,50]))
