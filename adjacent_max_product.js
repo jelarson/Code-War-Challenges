d0c38518ed77da7001d96920b56c9e10ec041cc9
jelarson <jesslarson93@gmail.com>
2020-11-13 10:40:03 -0700

    top answers added

--- adjacent_max_product.js
+++ adjacent_max_product.js
@@ -7,3 +7,16 @@ function adjacentElementsProduct(array) {
 console.log(adjacentElementsProduct([4, 12, 3, 1, 5]))
 console.log(adjacentElementsProduct([-23, 4, -5, 99, -27, 329, -2, 7, -921]))
 console.log(adjacentElementsProduct([1, 0, 1, 0, 1000]))
+
+// top answer
+// slice off last num ... duh!
+
+function adjacentElementsProduct(a) {
+  return Math.max(...a.map((x,i)=>x*a[i+1]).slice(0,-1))
+}
+
+// reducer
+
+const adjacentElementsProduct = (array) => array.slice(1).reduce(
+  (max, cur, i) => Math.max(array[i] * cur, max), -Infinity
+);
