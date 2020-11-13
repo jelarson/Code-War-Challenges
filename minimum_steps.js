092e217e82c8abc47838db4116b16aefd8f8ff60
jelarson <jesslarson93@gmail.com>
2020-11-13 11:20:31 -0700

    top answers added

--- minimum_steps.js
+++ minimum_steps.js
@@ -15,3 +15,41 @@ function minimumSteps(numbers, value){
 console.log(minimumSteps([4,6,3], 7))
 console.log(minimumSteps([8,9,10,4,2], 23))
 console.log(minimumSteps([19,98,69,28,75,45,17,98,67], 464))
+
+// top answers
+// sum variable added into for loop
+
+function minimumSteps(numbers, value) {
+  const nums = numbers.slice().sort((a, b) => a - b);
+  for (let i = 0, sum = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (sum >= value) {
+      return i;
+    }
+  }
+}
+
+// one liner
+
+const minimumSteps = (numbers, value) =>
+  numbers.sort((a, b) => a - b).filter(val => (value -= val) > 0).length;
+
+
+// while loop
+
+  function minimumSteps(numbers, value) {
+    numbers.sort((a, b) => a - b)
+    
+    var steps, sum, i
+    
+    steps = sum = i = 0
+    
+    while (true) {
+      sum += numbers[i]
+      if (sum >= value) break
+      steps++
+      i++
+    }
+    
+    return steps
+  }
