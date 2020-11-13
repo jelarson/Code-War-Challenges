2e1702243c24d70407bef5ae8b07accb83a1d23a
jelarson <jesslarson93@gmail.com>
2020-11-13 10:57:04 -0700

    max gap between sorted nums

---
+++ sorted_max_gap.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5a7893ef0025e9eb50000013/train/javascript
+
+function maxGap (numbers){
+  const sorted = numbers.sort((a,b)=>b-a)
+  return Math.max(...sorted.slice(1).map((num, idx)=> sorted[idx] - num))
+}
+
+console.log(maxGap([13,10,2,9,5]))
