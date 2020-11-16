247a551e302d3a2de5178f57dedf7822765190aa
jelarson <jesslarson93@gmail.com>
2020-11-16 11:57:20 -0700

    top answers added

--- disarium_nums.js
+++ disarium_nums.js
@@ -10,3 +10,32 @@ function disariumNumber(n){
 
 console.log(disariumNumber(89))
 console.log(disariumNumber(564))
+
+// top answers
+
+// similar logic - numArr can be left as a String, doesn't need to be arr to get idx
+
+function disariumNumber(n){
+  let number = n.toString();
+  let sum = 0;
+  
+  for (let i=0; i<number.length; i++){
+    sum += Math.pow(number[i], i+1);
+  }
+  
+  return n === sum ? "Disarium !!" : "Not !!";
+}
+
+// sudo one-liner
+
+const disariumNumber = n => `${n === n
+  .toString()
+  .split``
+  .reduce((r, e, i) => r + e ** (i + 1), 0) ?
+  'Disarium' : 
+  'Not'} !!`;
+
+// actual one liner with reducer
+
+const disariumNumber = n =>
+  [...`${n}`].reduce((pre, val, idx) => pre + val ** ++idx, 0) === n ? `Disarium !!` : `Not !!`;
