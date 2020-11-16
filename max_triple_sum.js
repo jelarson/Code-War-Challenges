901a5a676773438f429084908e5b2725f07f8fcc
jelarson <jesslarson93@gmail.com>
2020-11-16 12:07:34 -0700

    top answers added

--- max_triple_sum.js
+++ max_triple_sum.js
@@ -7,3 +7,18 @@ function maxTriSum(numbers){
 
 console.log(maxTriSum([2,9,13,10,5,2,9,5]))
 console.log(maxTriSum([2,1,8,0,6,4,8,6,2,4]))
+
+// top answers
+// cool way to name first 3 elements of array
+
+const maxTriSum = numbers => {
+  const [a,b,c,...rest] = [...new Set([...numbers])].sort((a,b)=>b-a)
+  return a+b+c
+}
+
+// slice and reduce - one liner
+
+function maxTriSum(numbers){
+  return ([...new Set(numbers)]).sort((a,b) => a-b).slice(-3).reduce((a,b) => a+b);
+}
+
