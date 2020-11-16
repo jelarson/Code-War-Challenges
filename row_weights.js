595de9f48db235102b4f6bc84c8cda48c105b8a1
jelarson <jesslarson93@gmail.com>
2020-11-16 11:33:49 -0700

    top answers added

--- row_weights.js
+++ row_weights.js
@@ -10,3 +10,28 @@ function rowWeights(array){
 }
 
 console.log(rowWeights([50,60,70,80]))
+
+// top answers
+
+// filter evens and odds, then reduce and return
+function rowWeights(array){
+  let t1 = array.filter((x, i)=>i%2==0).reduce((a,item)=>a+item,0);
+  let t2 = array.filter((x, i)=>i%2!=0).reduce((a,item)=>a+item,0);
+  
+  return [t1, t2]
+}
+
+// short answer 
+// one liner
+
+rowWeights=arr=>arr.reduce((a,b,i)=>(a[i%2]+=b,a),[0,0])
+
+// array instead of 2 variables
+
+function rowWeights(array){
+  var arr = [0, 0];
+  for (var i = 0; i < array.length; i++) {
+    i % 2 == 0 ? arr[0] += array[i] : arr[1] += array[i];
+  }
+  return arr;
+}
