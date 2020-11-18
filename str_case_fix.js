b8760e21801a179a747d5e8970c69b08d9436b89
jelarson <jesslarson93@gmail.com>
2020-11-17 18:33:10 -0700

    top answers added

--- str_case_fix.js
+++ str_case_fix.js
@@ -13,3 +13,24 @@ console.log(solve('code'))
 console.log(solve('coDe'))
 console.log(solve('cODe'))
 console.log(solve('CODe'))
+
+// top answers
+
+// regex
+
+const solve = s => s.replace(/[A-Z]/g,'').length < s.length/2 ? s.toUpperCase() : s.toLowerCase()
+
+// filter
+
+function solve(s){
+  let upper = s.split('').filter(x => x === x.toUpperCase()).length
+  let lower = s.length - upper
+  return (upper > lower) ? s.toUpperCase() : s.toLowerCase() 
+}
+
+// reduce with regex
+
+function solve(s){
+  return [...s].reduce((t,c)=>t+/[a-z]/.test(c)*2,0)<s.length?s.toUpperCase():s.toLowerCase();
+}
+
