89f38e968c1fe4686af0ff6eb82ee316e17fe760
jelarson <jesslarson93@gmail.com>
2020-11-18 10:31:28 -0700

    top answers added

--- char_type_count.js
+++ char_type_count.js
@@ -13,3 +13,40 @@ function solve(s){
  }
 
  console.log(solve("Codewars@codewars123.com"))
+
+ // top answer
+
+ // regex
+
+ const solve = (str) => (
+  [/[A-Z]/, /[a-z]/, /\d/, /[\W]/].map(rgx => str.split(rgx).length - 1)
+);
+
+// else if
+
+function solve(s){
+  let res = Array.from({length:4}, ()=> 0)
+  for(let v of s){
+    let vl = v.toLowerCase(), vu = v.toUpperCase()
+    if(!Number.isNaN(+v)) res[2]++
+    else if(vl === vu)    res[3]++
+    else if(v === vl)     res[1]++
+    else if(v === vu)     res[0]++
+  }
+  return res
+}
+
+// char code with for loop
+
+function solve(s){
+  let arr = [0, 0, 0, 0];
+  
+  for (let i = 0; i < s.length; i++) {
+    if (s.charCodeAt(i) >= 65 && s.charCodeAt(i) <= 90) arr[0]++;
+    if (s.charCodeAt(i) >= 97 && s.charCodeAt(i) <= 122) arr[1]++;
+    if (s.charCodeAt(i) >= 48 && s.charCodeAt(i) <= 57) arr[2]++;
+    if (s.charCodeAt(i) >= 33 && s.charCodeAt(i) <= 47) arr[3]++;
+    if (s.charCodeAt(i) >= 58 && s.charCodeAt(i) <= 64) arr[3]++;
+  }
+  return arr;
+}
