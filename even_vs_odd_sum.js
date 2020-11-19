ca999255a79a254d6e48322dbfff3126e58a9be9
jelarson <jesslarson93@gmail.com>
2020-11-19 12:38:27 -0700

    top answers added

--- even_vs_odd_sum.js
+++ even_vs_odd_sum.js
@@ -12,3 +12,49 @@ function evenOrOdd(str) {
 console.log(evenOrOdd('123'))
 console.log(evenOrOdd('1223'))
 console.log(evenOrOdd('12223'))
+
+// top answers
+
+// 2 filter loops
+function evenOrOdd(str) {
+  var odd = str.split('').filter(o => o % 2 != 0).reduce((a,b) => (+a) + (+b));
+  var even = str.split('').filter(e => e % 2 == 0).reduce((a,b) => (+a) + (+b));
+  if(even > odd) {
+    return 'Even is greater than Odd';
+  } else if(even < odd) {
+    return 'Odd is greater than Even';
+  } else {
+    return 'Even and Odd are the same';
+  }
+}
+
+// ternary inside reducer
+
+function evenOrOdd(str) {
+  
+  const diff = [ ...str ]
+    .map(Number)
+    .reduce((a, b) => b % 2 ? a - b : a + b, 0);
+  
+  if (diff > 0)
+    return 'Even is greater than Odd';
+  
+  if (diff < 0)
+    return 'Odd is greater than Even';
+  
+  return 'Even and Odd are the same';
+  
+}
+
+// one liner
+
+const evenOrOdd=s=>(x=eval(s.replace(/./g,x=>(+x%2?"+":"-")+x)))<0?'Even is greater than Odd':x>0?'Odd is greater than Even':'Even and Odd are the same'
+
+// regex
+
+function evenOrOdd(str) {
+  let [even, odd] = [/[13579]/g, /[02468]/g].map(re => str.replace(re, '').split``.reduce((s,i) => s + +i, 0))
+  if (even > odd) return 'Even is greater than Odd'
+  if (even < odd) return 'Odd is greater than Even'
+  return 'Even and Odd are the same'
+}
