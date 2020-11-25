1fe18c45a4c2c98e1794e2beac45ff319df2030f
jelarson <jesslarson93@gmail.com>
2020-11-25 10:21:45 -0800

    top answers added

--- expanded_nums.js
+++ expanded_nums.js
@@ -10,3 +10,38 @@ function expandedForm(num) {
 
 console.log(expandedForm(12))
 console.log(expandedForm(70304))
+
+// top answers
+
+// one liner
+
+const expandedForm = n => n.toString()
+                            .split("")
+                            .reverse()
+                            .map( (a, i) => a * Math.pow(10, i))
+                            .filter(a => a > 0)
+                            .reverse()
+                            .join(" + ");
+
+// reverse string and use for loop
+
+var expandedForm = (num) => {
+  var arr = num.toString().split('').reverse();
+  var result = [];
+  for(var i = 0; i < arr.length; i++){
+    arr[i] == 0 ? result.push() : result.push(arr[i] + ('0'.repeat(i)))
+  }
+  return result.reverse().join(' + ')
+}
+
+// map and regex to filter out 0's
+
+function expandedForm(num) {
+  var x = -1;
+  num = num.toString().split('').reverse().map(function(i) {
+    x++;
+    return i + '0'.repeat(x)
+  })
+  return num.filter((i => !(/^0+$/.test(i)))).reverse().join(' + ');
+}
+
