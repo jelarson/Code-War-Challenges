a63f10fceb8add4356c766bddaf1291078e131fd
jelarson <jesslarson93@gmail.com>
2020-11-25 10:39:59 -0800

    top answers added

--- window_ball_bounce.js
+++ window_ball_bounce.js
@@ -14,3 +14,38 @@ function bouncingBall(h,  bounce,  window) {
 
 console.log(bouncingBall(3.0, 0.66, 1.5))
 console.log(bouncingBall(30.0, 0.66, 1.5))
+
+// top answer
+
+// chain if and while loop
+
+function bouncingBall(h,  bounce,  window) {
+  var rebounds = -1;
+  if (bounce > 0 && bounce < 1) while (h > window) rebounds+=2, h *= bounce;
+  return rebounds;
+}
+
+// no while loop
+
+function bouncingBall(h, b, w) {
+  if (h <= w || b >= 1) return -1;
+  let r = Math.floor(Math.log(w/h)/Math.log(b)) * 2 + 1;
+  if (Number.isNaN(r)) return -1;
+  return r;
+}
+
+// recursive function
+
+function bouncingBall(h,  bounce,  window) {
+  if(h <= 0 || bounce <= 0 || bounce >= 1 || window >= h){
+    return -1;
+  }
+
+  var newHeight = h * bounce;
+  return bouncingBall(newHeight, bounce, window) + 2;
+}
+
+// recursive one liner
+
+let bouncingBall =f= (h,b,w) => h<=0 || b<=0 || b>=1 || w>=h ? -1 : 2 + f(h*b,b,w)
+
