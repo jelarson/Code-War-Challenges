838f5c92155cb5b28a605d112e1aa1ad0241b56d
jelarson <jesslarson93@gmail.com>
2020-11-24 17:46:29 -0800

    top answers added

--- consec_str.js
+++ consec_str.js
@@ -52,3 +52,46 @@ function longestConsec(strarr, k) {
 
 console.log(longestConsec(["zone", "abigail", "theta", "form", "libezzzz", "zas"], 2))
 console.log(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2))
+
+
+// top answers
+
+// same logic without early return
+
+function longestConsec(strarr, k) {
+  var longest = "";
+  for(var i=0;k>0 && i<=strarr.length-k;i++){
+    var tempArray = strarr.slice(i,i+k);
+    var tempStr = tempArray.join("");
+    if(tempStr.length > longest.length){
+      longest = tempStr;
+    }
+  }
+  return longest;
+}
+
+// reducer
+
+function longestConsec(strarr, k) {
+  if (k <= 0 || k > strarr.length) {
+    return '';
+  }
+  
+  return strarr.reduce((long, item, i) => {
+    const currString = strarr.slice(i, i + k).join('');
+    return (currString.length > long.length)
+      ? currString
+      : long;
+  }, '');
+}
+
+// ryan's answer with reducer
+
+function longestConsec(strarr, k) {
+  if(k < 1 || k > strarr.length) return ''
+  return strarr.reduce((longest, _, idx) => {
+    const str = strarr.slice(idx, idx + k).join('')
+    const length = str.length
+    return length > longest.length ? str : longest
+  }, '')
+}
