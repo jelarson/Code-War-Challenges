0d3585585858d767f28220aa96c845fa45b3b03f
jelarson <jesslarson93@gmail.com>
2020-11-30 09:52:34 -0700

    create astrik diamond based on n rows

--- diamond.js
+++ diamond.js
@@ -1,14 +1,20 @@
 // https://www.codewars.com/kata/5503013e34137eeeaa001648/train/javascript
 
 function diamond(n){
+  if (n%2 === 0 || n <= 0) return null
   const ans = [`${Array(n).fill('*').join('')}\n`]
   for (let i = n - 2; i >= 1; i-=2){
-    ans.push(Array(i).fill('*').join('') + '\n')
+    const arr = []
+    if (i < n) arr.push(' '.repeat((n-i)/2))
+    arr.push('*'.repeat(i))
+    ans.push(arr.join('') + '\n')
   }
-  return n%2 === 0? null : [...ans.slice(1).reverse(), ...ans].join(' ');
+  return [...ans.slice(1).reverse(), ...ans].join('');
 }
 
-console.log(diamond(1))
+// console.log(diamond(1))
 console.log(diamond(3))
-console.log(diamond(5))
-console.log(diamond(2))
+// console.log(diamond(5))
+// console.log(diamond(2))
+
+Expected: ' *\n***\n *\n', instead got: ' *\n ***\n  *\n'
