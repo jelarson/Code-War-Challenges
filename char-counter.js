219c7ab251adab9768dc0d9b4683b799fa39038e
jelarson <jesslarson93@gmail.com>
2020-12-01 20:20:32 -0700

    top answers added

--- char-counter.js
+++ char-counter.js
@@ -9,3 +9,35 @@ function count (string) {
 }
 
 console.log(count("aba"))
+
+// top answers
+
+// reducer 
+
+function count (string) {  
+  return string.split("").reduce(function(obj, elem) {
+    if (elem in obj)
+      obj[elem]++;
+    else
+      obj[elem] = 1;
+    return obj;
+  }, {});
+}
+
+// short answer same logic
+
+function count (string) {  
+  r = {};
+  string.split('').forEach(c => r[c] = (r[c] || 0) + 1);
+  return r;
+}
+
+// outside library
+
+const count = require('lodash').countBy;
+
+// one line reducer
+
+function count (string) {  
+  return string.split('').reduce(function(o,v){ o[v] ? o[v]++ : (o[v] = 1); return o; }, {});
+}
