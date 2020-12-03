69bc195a49b5b54dd5665fdd95b38695aae33ead
jelarson <jesslarson93@gmail.com>
2020-12-02 19:59:42 -0700

    top answers added

--- battle.js
+++ battle.js
@@ -23,3 +23,40 @@ function goodVsEvil(good, evil){
 }
 
 console.log(goodVsEvil('0 0 0 0 0 10', '0 1 1 1 1 0 0'))
+
+// top answers
+
+// reducer
+
+function goodVsEvil(good, evil) {  
+  var getWorth = function( side, worth ) {
+    return side.split(' ').reduce( function(result, value, index) { 
+      return result + (worth[index] * value);
+    }, 0);
+  }
+
+  var result = getWorth( good, [1,2,3,3,4,10] ) - getWorth( evil, [1,2,2,2,3,5,10] );
+
+  return result > 0 ? "Battle Result: Good triumphs over Evil" :
+         result < 0 ? "Battle Result: Evil eradicates all trace of Good" :
+                      "Battle Result: No victor on this battle field";
+}
+
+// point values in array to match up
+
+function goodVsEvil(good, evil) {
+  var worth = [ [1, 2, 3, 3, 4, 10], [1, 2, 2, 2, 3, 5, 10] ];
+  var good = good.split(' ').reduce((s,v,i) => s + worth[0][i] * v, 0);
+  var evil = evil.split(' ').reduce((s,v,i) => s + worth[1][i] * v, 0);
+  if (good > evil) return "Battle Result: Good triumphs over Evil";
+  else if (evil > good) return "Battle Result: Evil eradicates all trace of Good";
+  else return "Battle Result: No victor on this battle field";
+}
+
+// short answer
+
+const goodVsEvil = (good, evil) =>
+  (fn => `Battle Result: ${fn(good) === fn(evil) ? `No victor on this battle field` : fn(good) > fn(evil) ? `Good triumphs over Evil` : `Evil eradicates all trace of Good`}`)
+  (str => str.split(` `).reduce((pre, val, idx) => pre + val * (str === good ? [1, 2, 3, 3, 4, 10] : [1, 2, 2, 2, 3, 5, 10])[idx], 0));
+
+  
