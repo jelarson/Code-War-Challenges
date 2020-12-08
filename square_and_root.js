2f806078a79e6ca511e45c1e71c22bebc3e81911
jelarson <jesslarson93@gmail.com>
2020-12-08 07:21:34 -0700

    top answers added

--- square_and_root.js
+++ square_and_root.js
@@ -7,3 +7,41 @@ const sumSquareEvenRootOdd = ns => {
 };
 
 console.log(sumSquareEvenRootOdd([4,5,7,8,1,2,3,0]))
+
+// top answers
+
+// one liner with single reducer
+
+const sumSquareEvenRootOdd = ns => {
+  return +(ns.reduce((a, b) => a + (b % 2 ? b ** 0.5 : b ** 2), 0)).toFixed(2)
+};
+
+// tofixed(2) must round to two decimals
+
+const sumSquareEvenRootOdd = ns => {
+  // your code goes here
+  let tempNum = ns.map(element => {
+    return (element % 2 == 0 ? element * element : Math.sqrt(element));
+  })
+  
+  let sum = tempNum.reduce((a, b) => a + b, 0);
+  
+  return sum.toFixed(2)*1;
+};
+
+// for loop
+
+const sumSquareEvenRootOdd = ns => { 
+  var lista = [], somma = 0;
+  
+  for(i=0;i<ns.length;i++){
+    if(ns[i] % 2 == 0){
+      somma += ns[i]**2;
+    } else {
+      somma += Math.sqrt(ns[i]);
+    }
+  }
+
+  somma = Math.round(somma*100)/100;
+  return somma;
+};
