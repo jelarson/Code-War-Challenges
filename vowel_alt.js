6e83b52d9e68cd9eb6a17cd793fe182cc98f058a
jelarson <jesslarson93@gmail.com>
2020-12-09 18:33:02 -0700

    top answers added

--- vowel_alt.js
+++ vowel_alt.js
@@ -32,3 +32,57 @@ function solve(s) {
 //  console.log(solve('oruder'))
  console.log(solve('apple'))
  console.log(solve('java'))
+
+ // top answers
+
+ function solve(s) {
+
+  let a = s.match(/[^aeiou]/g).sort();
+  let b = s.match(/[aeiou]/g).sort();
+  let r = '';
+  
+  if (a.length > b.length)
+    [a, b] = [b, a];
+    
+  if (b.length - a.length > 1)
+    return 'failed';
+    
+  return b
+    .map((c, i) => c + (a[i] || ''))
+    .join('');
+  
+};
+
+// lo dash
+
+const {zip} = require('lodash')
+
+function solve(s){
+  let c = (s.match(/[^aeiou]/g) || []).sort(), v = (s.match(/[aeiou]/g) || []).sort()
+  if (Math.abs(c.length - v.length) > 1) return 'failed'
+  let zipped = c.length > v.length ? zip(c, v) : zip(v, c)
+  return zipped.map(x => x.filter(a => a).join``).join``
+}
+
+// ryans answer
+
+function solve(s) {
+  const word = s.split('')
+  const vowels = ['a', 'e', 'i', 'o', 'u']
+  const [vow, con] = word.reduce(([v, c], next) => {
+      if(vowels.includes(next)) return [[...v, next], [...c]]
+      return [[...v], [...c, next]]
+  }, [[], []])
+  vow.sort()
+  con.sort()
+  const numVowels = word.reduce((a, b) => a + (vowels.includes(b) ? 1 : 0), 0)
+  const count = (word.length - numVowels) - numVowels
+  if(![-1, 0, 1].includes(count)) return 'failed'
+  const [more, less] = count < 1 ? [vow, con] : [con, vow]
+  let solution = []
+  for(let i = 0; i < word.length; i++){
+    const which = i % 2 ? less : more
+    solution.push(which.shift())
+  }
+  return solution.join('')
+};
