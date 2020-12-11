9138a34ea3cd71bd4c7eca94feab042d79922d6b
jelarson <jesslarson93@gmail.com>
2020-12-10 18:24:30 -0700

    top answers added

--- bugfix.js
+++ bugfix.js
@@ -16,3 +16,17 @@ function yourFutureCareer() {
 const arr = ['FrontEnd Developer', 'BackEnd Developer', 'Full-Stack Developer']
 
 const yourFutureCareer = () => arr[Math.floor(Math.random()*3)]
+
+// top answers
+
+const yourFutureCareer = () => {
+  let career = Math.random();
+  return `${ career <= 0.32 ? 'FrontEnd' : (career <= 0.65 ? 'BackEnd' : 'Full-Stack') } Developer`;
+}
+
+const randomChoice = arr => arr[Math.floor(Math.random() * arr.length)];
+const yourFutureCareer = () =>randomChoice(['FrontEnd Developer', 'BackEnd Developer', 'Full-Stack Developer']);
+
+// ryan's answer
+
+const yourFutureCareer = () => ['FrontEnd Developer', 'BackEnd Developer', 'Full-Stack Developer'][Math.floor(Math.random() * 3)]
