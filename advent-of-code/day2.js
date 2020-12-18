10b7042d024cbdc98a5d18168134ae80fb36659c
jelarson <jesslarson93@gmail.com>
2020-12-17 21:44:34 -0700

    day 2 part 2 solution

--- advent-of-code/day2.js
+++ advent-of-code/day2.js
@@ -19,9 +19,20 @@ fs.readFile(path1, {encoding: 'utf-8'}, (err, data) => {
     })
     return count
   }
-  console.log(day2(arr))
+  // console.log(day2(arr))
   // console.log(arr[2])
   // const str='2-4 g: jgzq'
   // console.log([Number(str.split('-')[0]), Number(str.split('-')[1].split(' ')[0]), str.split('-')[1].split(' ')[1].split(':')[0], str.split('-')[1].split(' ')[2]])
-
+  function day2Part2(arr){
+    let count = 0
+    arr.forEach(itm=>{
+      const itmArr = [Number(itm.split('-')[0]), Number(itm.split('-')[1].split(' ')[0]), itm.split('-')[1].split(' ')[1].split(':')[0], itm.split('-')[1].split(' ')[2]]
+      let letCount = 0
+      if (itmArr[3][itmArr[0] -1] === itmArr[2]) letCount++
+      if (itmArr[3][itmArr[1] -1] === itmArr[2]) letCount++
+      if (letCount === 1) count++
+    })
+    return count
+  }
+  console.log(day2Part2(arr))
 })
