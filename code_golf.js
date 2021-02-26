265fd9785e18a8d29b4206f5e77c2109efae2aa1
jelarson <jesslarson93@gmail.com>
2021-02-25 22:09:57 -0700

    shortest code - always return odd number

---
+++ code_golf.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/5f882dcc272e7a00287743f5/train/javascript
+
+const alwaysOdd=n=>n**2%2==1?n:n-1
+const alwaysOdd=n=>n%2?n:n-1
+
+console.log(alwaysOdd(1))
+console.log(alwaysOdd(3))
+console.log(alwaysOdd(-3))
+console.log(alwaysOdd(4))
