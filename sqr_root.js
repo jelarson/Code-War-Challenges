daaef46f86efa323b9093150b1a29b9c7879717b
jelarson <jesslarson93@gmail.com>
2021-03-10 21:10:53 -0700

    find sqr root without Math.sqrt, rounded to 5 places

---
+++ sqr_root.js
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/56426b4e971f01fd2f00005b/train/javascript
+
+function squareRoot(x) {
+  return Math.round(x**.5*100000)/100000
+}
+
+console.log(squareRoot(15))
+console.log(squareRoot(21))
