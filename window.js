ba6b57919b1d089c23b2063490084abff615111f
jelarson <jesslarson93@gmail.com>
2021-03-12 23:01:49 -0700

    top answers added

--- window.js
+++ window.js
@@ -12,3 +12,16 @@ function makeAWindow(num) {
 }
 
 console.log(makeAWindow(3))
+
+//top answers
+
+function makeAWindow(num) {
+  var side = `|${'.'.repeat(num)}|${'.'.repeat(num)}|\n`.repeat(num)
+  var centre = `|${'-'.repeat(num)}+${'-'.repeat(num)}|\n`
+  return '-'.repeat(num * 2 + 3) + '\n' + side + centre + side + '-'.repeat(num * 2 + 3) 
+}
+
+// short answer
+makeAWindow=(n,s='-'.repeat(n),d='.'.repeat(n),f=`-${s}-${s}-`,r=`|${d}|${d}|`,w=Array(n).fill(r))=>[f,...w,`|${s}+${s}|`,...w,f].join`\n`
+
+
