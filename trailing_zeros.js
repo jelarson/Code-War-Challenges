13f4135d5ac637bcadd052f7da54ff055356ee28
jelarson <jesslarson93@gmail.com>
2021-06-01 21:32:33 -0600

    get rid of trailing zeros

---
+++ trailing_zeros.js
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/570a6a46455d08ff8d001002/train/javascript
+
+function noBoringZeros(n) {
+  const numArr = String(n).split('').reverse()
+  for(let i = 0; i < numArr.length; i++){
+    if (numArr[i] == '0'){
+      numArr[i] = '#'
+    } else {
+      i = n.length;
+    }
+  }
+  return Number(numArr.reverse().join('').split('#').join(''))
+}
+
+console.log(noBoringZeros(1450))
+console.log(noBoringZeros(-1450))
