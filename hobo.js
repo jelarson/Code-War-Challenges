002fd2fc5fd9c6730f15d1caf7db26b50ab239d2
jelarson <jesslarson93@gmail.com>
2021-06-09 23:58:11 -0600

    with array logic

--- hobo.js
+++ hobo.js
@@ -2,5 +2,6 @@
 
 function bald(x){
   const num = x.split('').filter(b=>b=='/').length
-  return num == 0 ? "Clean!" : num == 1 ? "Unicorn!" : num == 2 ? "Homer!" : num >5 ? "Hobo!" : "Careless!"
+  const term = num == 0 ? "Clean!" : num == 1 ? "Unicorn!" : num == 2 ? "Homer!" : num >5 ? "Hobo!" : "Careless!"
+  return [Array(x.length).fill('-').join(''), term]
 }
