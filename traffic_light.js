cecc1b912af079da0809712b83703d9cdf133644
jelarson <jesslarson93@gmail.com>
2021-07-06 22:45:29 -0600

    What should the traffic light change to?

---
+++ traffic_light.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/58649884a1659ed6cb000072/train/javascript
+
+function updateLight(current) {
+  return current == 'green' ? 'yellow' : current == 'red' ? 'green' : 'red'
+}
