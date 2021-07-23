7b36bdf4d4e89f9aa05501c9ce5f6192c2515114
jelarson <jesslarson93@gmail.com>
2021-07-23 12:20:30 -0600

    What's between the two

---
+++ between.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/55ecd718f46fba02e5000029/train/javascript
+
+function between(a, b) {
+  const arr = []
+  for (let i = a; i <= b; i++){
+    arr.push(i)
+  }
+  return arr
+}
