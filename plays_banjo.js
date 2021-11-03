c2ff32dc98e56104d6a44fdc3784c39f1905e2b0
jelarson <jesslarson93@gmail.com>
2021-11-02 20:51:45 -0600

    do you play banjo?

---
+++ plays_banjo.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/53af2b8861023f1d88000832/train/javascript
+
+function areYouPlayingBanjo(name) {
+  return name.toLowerCase()[0] == 'r' ? name + ' plays banjo' : name + ' does not play banjo';
+}
