9593928dbc22840904924970118f7d9a3876e2c6
jelarson <jesslarson93@gmail.com>
2021-12-02 22:30:38 -0700

    hello in foreign language

---
+++ foreign_hello.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/56a4addbfd4a55694100001f/train/javascript
+
+function validateHello(greetings) {
+  let arr = ['hello','ciao','salut','hallo','hola','ahoj','czesc']
+  for (let i = 0; i < arr.length; i++){
+    if (greetings.toLowerCase().includes(arr[i])){
+      return true
+    }
+  }
+  return false
+}
