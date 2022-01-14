2c2309698c6ad7eef2e10a2fdd84735c4a6ff8d8
jelarson <jesslarson93@gmail.com>
2022-01-13 23:33:14 -0700

    create escape room object

---
+++ escape_rooms.js
@@ -0,0 +1,20 @@
+// https://www.codewars.com/kata/56a29b237e9e997ff2000048/train/javascript
+
+// Add rooms here
+var rooms = {
+  room1 : {
+    name :'name',
+    description :'description',
+    solution :'solution'
+  },
+  room2 : {
+    name :'name',
+    description : 'description',
+    solution : 'solution'
+  },
+  room3 : {
+    name : 'name',
+    description : 'description',
+    solution : 'solution'
+  }
+}
