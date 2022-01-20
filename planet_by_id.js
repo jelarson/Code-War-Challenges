6160fdf7326ba18508125ec8a0fa205640c7588b
jelarson <jesslarson93@gmail.com>
2022-01-19 20:53:48 -0700

    return planet by id

---
+++ planet_by_id.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/515e188a311df01cba000003/train/javascript
+
+function getPlanetName(id){
+  return ['Mercury','Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune'][id -1]
+}
