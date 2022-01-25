18949ca37899d6015ca788df2237d41ffc192cde
jelarson <jesslarson93@gmail.com>
2022-01-24 23:09:51 -0700

    return age difference array

---
+++ age_diff_array.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/5720a1cb65a504fdff0003e2/train/javascript
+
+function differenceInAges(ages){
+  return [Math.min(...ages), Math.max(...ages), Math.max(...ages) - Math.min(...ages)]
+}
