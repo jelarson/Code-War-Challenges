d03f14971a64b633f3f82a132d88467f73c6460c
jelarson <jesslarson93@gmail.com>
2022-02-14 21:56:16 -0700

    return ufc quote

---
+++ ufc.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/582dafb611d576b745000b74/train/javascript
+
+var quote = function(fighter) {
+  return fighter.toLowerCase() == 'conor mcgregor' ? "I'd like to take this chance to apologize.. To absolutely NOBODY!" : "I am not impressed by your performance."
+};
