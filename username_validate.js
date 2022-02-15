98b0f0e8c3e6599e65ae6a17d45255541df97a47
jelarson <jesslarson93@gmail.com>
2022-02-15 10:25:53 -0700

    validate the username

---
+++ username_validate.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/56a3f08aa9a6cc9b75000023/train/javascript
+
+function validateUsr(username) {
+  console.log(username)
+  res =  /^[a-z0-9_]+$/i.test(username)
+  return res && username.length > 3 && username.length < 17 && username === username.toLowerCase()
+}
