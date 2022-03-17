47164e465ed3005224cbed51f3a1c8534d5d1ccf
jelarson <jesslarson93@gmail.com>
2022-03-16 19:32:19 -0600

    another solution

--- days_in_month.js
+++ days_in_month.js
@@ -4,3 +4,8 @@ function howManydays(month){
   // switch
   return month == 2 ? 28 : [4,6,9,11].includes(month) ? 30 : 31;
 }
+
+function howManydays(month){
+  // switch
+  return [0,31,28,31,30,31,30,31,31,30,31,30,31][month]
+}
