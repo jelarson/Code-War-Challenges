8bf8e697f5613406aebee4b4072414be6babfe8a
jelarson <jesslarson93@gmail.com>
2022-06-08 23:52:06 -0600

    root or square nums in array

---
+++ root_square.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/57f6ad55cca6e045d2000627/train/javascript
+
+function squareOrSquareRoot(array) {
+  return array.map(x=>{
+    return x**.5 % 1 > 0 ? x**.5 : x**2
+  });  
+}
