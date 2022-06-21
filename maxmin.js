5dd8e1667ec4d1e4fcc900fd242fd7d067207740
jelarson <jesslarson93@gmail.com>
2022-06-21 00:02:07 -0600

    max and min of diffs between arrays

--- maxmin.js
+++ maxmin.js
@@ -1,6 +1,6 @@
 // https://www.codewars.com/kata/5733d6c2d780e20173000baa/train/javascript
 
 function maxMin(arr1,arr2){
-  //coding here...
-  
+  var arr = arr1.map((x,i)=>Math.abs(x-arr2[i]))
+  return [Math.max(...arr), Math.min(...arr)]
 }
