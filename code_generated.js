9d1da04350b0772c78f2eec8843379d8d31bb092
jelarson <jesslarson93@gmail.com>
2022-06-28 22:59:28 -0600

    top answer added

--- code_generated.js
+++ code_generated.js
@@ -6,3 +6,12 @@ function rndCode(){
   var symbols = '~!@#$%^&*';
   return letters[Math.floor(Math.random()*letters.length - 1)] + letters[Math.floor(Math.random()*letters.length - 1)] + numbers[Math.floor(Math.random()*numbers.length - 1)] + numbers[Math.floor(Math.random()*numbers.length - 1)] + numbers[Math.floor(Math.random()*numbers.length - 1)] + numbers[Math.floor(Math.random()*numbers.length - 1)] + symbols[Math.floor(Math.random()*symbols.length - 1)] + symbols[Math.floor(Math.random()*symbols.length - 1)]
 }
+
+function rndCode(){
+  
+  var p1 = 'ABCDEFGHIJKLM';
+  var p2 = '0123456789';
+  var p3 = '~!@#$%^&*';
+  return [p1,p1,p2,p2,p2,p2,p3,p3].map((v) => v[~~(Math.random() * v.length)]).join('');
+
+}
