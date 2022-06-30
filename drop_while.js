1f4581625523d6b0bcc457b071a83ba46bab6cb1
jelarson <jesslarson93@gmail.com>
2022-06-29 21:50:44 -0600

    drop while function true

---
+++ drop_while.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/54f9c37106098647f400080a/train/javascript
+
+function dropWhile(arr, pred) {
+  var bool = true;
+  return arr.filter(x=>{
+    if (bool) {
+      bool = pred(x);
+      return !pred(x)
+    } else {
+      return true;
+    }
+  })
+}
