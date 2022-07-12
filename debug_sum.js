fa47b4f3da5d0939f613e58441d4c4364b302ee4
jelarson <jesslarson93@gmail.com>
2022-07-12 12:37:28 -0600

    personal answer - withut debug

--- debug_sum.js
+++ debug_sum.js
@@ -1,10 +1,14 @@
 // https://www.codewars.com/kata/563d59dd8e47a5ed220000ba/train/javascript
 
 function getSumOfDigits(integer) {
+  return Number(String(integer).split('').reduce((a,b)=>Number(a) + Number(b)))
+}
+
+function getSumOfDigits(integer) {
   var sum = null;
   var digits =  Math.floor(integer).toString();
   for(var ix = 1; ix < digits.length; ix = sum + 1) {
-    sum =+ digits[ix + 1]);
+    sum += digits[ix + 1];
   }
   return sum;
 }
