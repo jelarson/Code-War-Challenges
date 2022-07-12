ee98e395a1e85e5eca2c282b0a6c16337437e932
jelarson <jesslarson93@gmail.com>
2022-07-11 23:27:15 -0600

    Debug num sum

---
+++ debug_sum.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/563d59dd8e47a5ed220000ba/train/javascript
+
+function getSumOfDigits(integer) {
+  var sum = null;
+  var digits =  Math.floor(integer).toString();
+  for(var ix = 1; ix < digits.length; ix = sum + 1) {
+    sum =+ digits[ix + 1]);
+  }
+  return sum;
+}
