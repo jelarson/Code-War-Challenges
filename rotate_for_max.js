691ea5baf8a9fd65e0b67e28e8b0187d48c53767
jelarson <jesslarson93@gmail.com>
2022-07-19 21:19:46 -0600

    rotate for max

--- rotate_for_max.js
+++ rotate_for_max.js
@@ -1,14 +1,12 @@
 // https://www.codewars.com/kata/56a4872cbb65f3a610000026/train/javascript
 
-function maxRot(n) {
-  var num = n + '';
-  let ans = '';
-  for (let i = 0; i < num.length; i ++) {
-    if () {
-      
-    } else {
-      i = num.length
-    }
+function maxRot(n){
+  let ans = n
+  let arr = String(n).split('')
+  for(let i = 0; i < arr.length; i++){
+    arr.push(arr.splice(i,1))
+    const num = Number(arr.join(''))
+    if(num > ans) ans = num
   }
   return ans
 }
