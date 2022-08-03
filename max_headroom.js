62deb2b4a5c6a3fdf9057a3b497a20e860e6a7bb
jelarson <jesslarson93@gmail.com>
2022-08-02 19:18:03 -0600

    functions should return same

---
+++ max_headroom.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/52a47dd7e950edabfa000391/train/javascript
+
+function getMax1()
+{
+  var max = 
+  {
+   name: 'Max Headroom'
+  }
+  return max;
+}
+
+function getMax2()
+{
+  return getMax1();
+}
