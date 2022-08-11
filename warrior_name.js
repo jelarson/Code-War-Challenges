2cb5df3b834a6f9780b30fa07025d666cac13dd0
jelarson <jesslarson93@gmail.com>
2022-08-10 23:59:52 -0600

    top answer

--- warrior_name.js
+++ warrior_name.js
@@ -11,3 +11,16 @@ function Warrior(n){
 Warrior.prototype.toString = function(){
     return "Hi! my name's "+this.name();
 }
+
+function Warrior(n){
+  this.n = n;
+}
+
+Warrior.prototype.name = function(n){
+  if (n) this.n = n;
+  return this.n;
+}
+
+Warrior.prototype.toString = function(){
+  return "Hi! my name's " + this.n;
+}
