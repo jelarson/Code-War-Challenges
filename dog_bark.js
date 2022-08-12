0b1fd6ed1c31addb2ca06369c6b341242032f5a5
jelarson <jesslarson93@gmail.com>
2022-08-12 12:55:43 -0600

    Easy method

--- dog_bark.js
+++ dog_bark.js
@@ -15,3 +15,16 @@ var scoobydoo = new Dog("Great Dane");
 scoobydoo.bark = function() {
   return "Woof";
 };
+
+class Dog {
+  constructor(breed) {
+      this.breed = breed;
+  }
+  
+  bark() {
+      return "Woof";
+  }
+}
+
+let snoopy = new Dog("Beagle");
+let scoobydoo = new Dog("Great Dane");
