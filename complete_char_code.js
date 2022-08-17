fdb38cbe9f1492d943d8b28d6d6d625d1c52f8b2
jelarson <jesslarson93@gmail.com>
2022-08-16 21:33:20 -0600

    complete char code function

---
+++ complete_char_code.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/559f3123e66a7204f000009f/train/javascript
+
+var ArrowFunc = function(arr) {
+  return arr.map(x => String.fromCharCode(x)).join(''); //Complete this function
+}
