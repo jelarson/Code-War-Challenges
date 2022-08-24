ed0dc127a149decb2dff3dfbfbafe890ee3b6a8f
jelarson <jesslarson93@gmail.com>
2022-08-23 23:55:23 -0600

    Complete the person object

---
+++ person_obj.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/55a13556ca4a6d0ab4000003/train/javascript
+
+var Person = function(){
+  var person = {
+    _name: "Leroy",
+    _friends: [],
+    fillFriends(f) { }
+  }
+  return person;
+}
