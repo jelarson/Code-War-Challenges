fd8a494cbd7687bce7bcabcc25aff6ac48bf5bd9
jelarson <jesslarson93@gmail.com>
2022-09-05 23:11:45 -0600

    Power exponent

---
+++ power.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/562926c855ca9fdc4800005b/train/javascript
+
+// can't use Math.pow()
+function numberToPower(number, power){
+  return Math.pow(number, power)
+}
+
+// can't use **
+function numberToPower(number, power){
+  return number ** power;
+}
