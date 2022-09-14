868fedadf8a97bef9b4982c3b5b37b91efc6dec8
jelarson <jesslarson93@gmail.com>
2022-09-13 20:56:53 -0600

    round to dynamic precision

---
+++ round_to.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/56be025f9347a066c7000e4f/train/javascript
+
+Math.roundTo = function (number, precision) {
+  return Math.round(number * 10**precision) / 10**precision
+}
