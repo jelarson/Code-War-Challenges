7b7a98f159e72812946f39bdfa9d89c0d9955e77
jelarson <jesslarson93@gmail.com>
2022-10-13 20:42:06 -0600

    one liner

--- bac.js
+++ bac.js
@@ -4,3 +4,5 @@ function bloodAlcoholContent(drinks, weight, sex, time){
   let r = sex == 'male' ? .73 : .66
   return Number(((drinks.ounces * drinks.abv * 5.14/ weight * r) - .015 * time).toFixed(4));
 }
+
+var bloodAlcoholContent=(d,w,s,t)=>+(((d.ounces*d.abv*5.14/w*(s=='male'?0.73:0.66))-.015*t).toFixed(4))
