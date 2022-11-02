473ed32be601463f60b15a5f179b932abf7672e9
jelarson <jesslarson93@gmail.com>
2022-11-01 23:12:51 -0600

    switch case

--- oscars.js
+++ oscars.js
@@ -6,3 +6,17 @@ function leo(oscar){
   else if (oscar == 86) return 'Not even for Wolf of wallstreet?!'
   else return 'When will you give Leo an Oscar?'
 }
+
+function leo(oscar){
+  switch (oscar){
+    case 88:
+      return "Leo finally won the oscar! Leo is happy";
+      break;
+    case 86:
+      return  "Not even for Wolf of wallstreet?!";
+      break;
+    default:
+      if(oscar<88) return "When will you give Leo an Oscar?";
+      else return  "Leo got one already!";
+  }
+}
