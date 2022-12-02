90e89ed3d5ec2e322ac275fec9a3dc7b83d01db1
jelarson <jesslarson93@gmail.com>
2022-12-01 21:15:03 -0700

    adjust lives for number guess game

---
+++ num_guess.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/568018a64f35f0c613000054/train/javascript
+
+class Guesser {
+  constructor(number, lives) {
+    this.number = number;
+    this.lives = lives;
+  }
+  
+  guess(n) {
+    return false;
+  }
+}
