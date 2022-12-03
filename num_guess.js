4f068e8d44302410caaf64b9dcc8d81dd226caf1
jelarson <jesslarson93@gmail.com>
2022-12-02 21:58:12 -0700

    top answer

--- num_guess.js
+++ num_guess.js
@@ -10,3 +10,16 @@ class Guesser {
     return false;
   }
 }
+
+class Guesser {
+  constructor(number, lives) {
+    this.number = number;
+    this.lives = lives;
+  }
+  
+  guess(n) {
+    if (!this.lives) throw Error
+    this.lives--
+    return n === this.number
+  }
+}
