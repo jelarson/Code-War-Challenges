f1dfddf25ebf21ec8a361728eed6994ea8508679
jelarson <jesslarson93@gmail.com>
2022-12-28 18:27:51 -0700

    one liner

--- grader.js
+++ grader.js
@@ -3,3 +3,5 @@
 function grader(s) {
   return s < 0.6 || s > 1 ? "F" : s < 0.7 ? "D" : s < 0.8 ? "C" : s < 0.9 ? "B" : "A" 
 }
+
+grader = s => s > 1 || s < 0.6 ? 'F' : s < 0.7 ? 'D' : s < 0.8 ? 'C' : s < 0.9 ? 'B' : 'A';
