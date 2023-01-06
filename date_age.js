8917e5727df787a631631ee1682a5ad3aa9a6019
jelarson <jesslarson93@gmail.com>
2023-01-05 21:13:36 -0700

    one liner

--- date_age.js
+++ date_age.js
@@ -3,3 +3,5 @@
 function datingRange(age){
   return age > 14 ? Math.floor((age / 2)) + 7 + '-' + (age - 7) * 2 : Math.floor(age * .9) + '-' + Math.floor(age * 1.1)
 }
+
+datingRange = a => a > 14 ? `${~~(a / 2) + 7}-${a * 2 - 14}` : `${~~(a * 0.9)}-${~~(a * 1.1)}`;
