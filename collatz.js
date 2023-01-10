4cdd92b61503141fab27c47c292646a69d3fd04c
jelarson <jesslarson93@gmail.com>
2023-01-09 20:59:36 -0700

    top answer

--- collatz.js
+++ collatz.js
@@ -10,3 +10,11 @@ var hotpo = function(n){
     }
     
 }
+
+var hotpo = function(n, acc = 0) {
+  if (n <= 1) {
+    return acc;
+  } else {
+    return hotpo(n%2==0 ? n/2 : 3*n+1, acc+1);
+  }
+}
