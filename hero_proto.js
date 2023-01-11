2f12baadfd63895951c015d7871b76133d157dd3
jelarson <jesslarson93@gmail.com>
2023-01-10 21:51:37 -0700

    declare a hero

---
+++ hero_proto.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/55e8aba23d399a59500000ce/train/javascript
+
+function Hero (name) {
+  this.name = name || 'Hero';
+  this.position = '00';
+  this.health = 100;
+  this.damage = 5;
+  this.experience = 0;
+}
