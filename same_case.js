c5b3bca64199f5e26c64ede180f723d11284b2c6
jelarson <jesslarson93@gmail.com>
2023-01-17 21:33:59 -0700

    top answer

--- same_case.js
+++ same_case.js
@@ -3,3 +3,13 @@
 function sameCase(a, b){
   return (a.toUpperCase() === a && b.toUpperCase() === b) || (a.toLowerCase() === a && b.toLowerCase() === b) ? 1 : 0;
 }
+
+function sameCase(a, b){
+ if(a.toUpperCase() === a.toLowerCase() || b.toLowerCase() === b.toUpperCase()){
+    return -1
+  }else if(a === a.toLowerCase() && b === b.toLowerCase() || a === a.toUpperCase() && b === b.toUpperCase()){
+      return 1
+  }else{
+    return 0
+  }
+}
