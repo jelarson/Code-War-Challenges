69d87d479c4004ca2603261dd98dd96343b8a775
jelarson <jesslarson93@gmail.com>
2023-01-20 19:05:27 -0700

    top answer

--- duck_goose.js
+++ duck_goose.js
@@ -3,3 +3,9 @@
 function duckDuckGoose(players, goose) {
   return players[goose % players.length - 1]
 }
+
+const duckDuckGoose = (players, goose) => players[(goose - 1) % players.length].name;
+
+function duckDuckGoose(players, goose) {
+  return players[(goose - 1) % players.length].name;
+}
