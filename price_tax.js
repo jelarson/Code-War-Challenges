c9185dffd5cc879a9733d3c2d60375e1b1fd884c
jelarson <jesslarson93@gmail.com>
2023-02-01 06:48:47 -0700

    price without tax

---
+++ price_tax.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/5890d8bc9f0f422cf200006b/train/javascript
+
+function excludingVatPrice(price){
+  return price || price === 0 ? Math.round(price * 100 / 1.15) / 100 : -1;
+}
