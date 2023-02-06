27c257d049d59a3714e683e62bf290b2cef8c430
jelarson <jesslarson93@gmail.com>
2023-02-06 00:52:54 -0700

    remove the exclamation point at the end

---
+++ last_exclamation.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/57fae964d80daa229d000126/train/javascript
+
+function remove (string) {
+  return string[string.length - 1] == '!' ? string.slice(0,string.length - 1) : string;
+}
