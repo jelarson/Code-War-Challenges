b8ac7211b0dc87222ab7f75c3a7251d517a2a20f
jelarson <jesslarson93@gmail.com>
2023-02-15 09:01:34 -0700

    without for loop

--- gas_litre.js
+++ gas_litre.js
@@ -8,3 +8,9 @@ function fuelPrice(litres, pricePerLiter) {
   }
   return Math.round(litres * pricePerLiter * 100) / 100;;
 }
+
+function fuelPrice(litres, pricePerLiter) {
+  var discount = Math.min(Math.floor(litres/2) * 0.05, 0.25);
+  var price = litres * (pricePerLiter - discount);
+  return Math.round(price*100) / 100;
+}
