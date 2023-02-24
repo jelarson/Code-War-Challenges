035ad14f1beca34ca81f440c0f282d40ca903812
jelarson <jesslarson93@gmail.com>
2023-02-24 00:56:43 -0700

    top answer

--- cube_paint.js
+++ cube_paint.js
@@ -14,3 +14,6 @@ countSquares=n=>n?6*n*n+2:1
 var countSquares = function(cuts){
   return (cuts ? 6 * Math.pow(cuts, 2) + 2 : 1)
 }
+
+const countSquares = cuts =>
+  (6 * cuts ** 2 + 2) ** !!cuts;
