2e66ce19ab4315f19494cf3e9dbb61c9ab64a71c
jelarson <jesslarson93@gmail.com>
2023-02-27 19:43:06 -0700

    remove vowels from string

---
+++ vowel_rmv.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/5547929140907378f9000039/train/javascript
+
+function shortcut (string) {
+  return [...string].filter(x=>!['a', 'e', 'i', 'o', 'u'].includes(x)).join('');
+}
