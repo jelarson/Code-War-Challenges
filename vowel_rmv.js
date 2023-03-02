b685449e7815ffb5e14ff500286bbbc21fcba044
jelarson <jesslarson93@gmail.com>
2023-03-02 07:15:32 -0700

    for loop with indexOf()

--- vowel_rmv.js
+++ vowel_rmv.js
@@ -11,3 +11,14 @@ function shortcut (string) {
 function shortcut(string){
   return string.replace(/[aeiou]/gi, '');
 }
+
+function shortcut(string){
+  var vowels = "aeiou";
+  var output = "";
+  for (var i = 0; i < string.length; i++) {
+    if (!(vowels.indexOf(string[i]) > -1)) {
+      output += string[i];
+    }
+  }
+  return output;
+}
