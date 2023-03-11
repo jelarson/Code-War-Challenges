d276e4890f59e4a677bf036b71557968ff0deab3
jelarson <jesslarson93@gmail.com>
2023-03-10 23:28:56 -0700

    popular answer

--- arr_methods.js
+++ arr_methods.js
@@ -6,3 +6,9 @@ function blackAndWhite(arr){
 
 const blackAndWhite = arr =>
   `It's a ${!Array.isArray(arr) ? `fake` : arr.includes(5) && arr.includes(13) ? `black` : `white`} array`;
+
+  function blackAndWhite(arr){
+    if ( !Array.isArray( arr ) ) return 'It\'s a fake array'
+    if ( arr.indexOf( 5 ) != -1 && arr.indexOf( 13 ) != -1 ) return 'It\'s a black array'
+    return 'It\'s a white array'
+  }
